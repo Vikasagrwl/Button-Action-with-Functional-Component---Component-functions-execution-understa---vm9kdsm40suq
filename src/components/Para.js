e3421f90df8db320a393ef3255ccbf5d15efb4c4
Vikasagrwl <105685053+Vikasagrwl@users.noreply.github.com>
2023-02-24 16:09:43 +0530

    Update Para.js

--- src/components/Para.js
+++ src/components/Para.js
@@ -2,14 +2,15 @@ import React from "react";
 import { useState } from "react";
 
 export default function Para(){
-    const[msg, setMsg]=useState("");
+    const[msg, setMsg]=useState(false);
     function handleClick(){
-        setMsg("Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy")
+        setMsg(true)
     }
     return(
         <div>
             <button id="click" onClick={handleClick}>click me</button>
-            <p>{msg}</p>
+            {msg? <p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>: ""}
+            
         </div>
     )
 }
